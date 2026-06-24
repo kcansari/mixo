c7e8b9a0d6f1bebb25a3708a03f123cfaad3f462
kcansari <kcansari0@gmail.com>
2026-06-24 09:41:38 +0300

    init project

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+	slog.Info("Server started on :3333")
+	http.ListenAndServe(":3333", r)
+}
